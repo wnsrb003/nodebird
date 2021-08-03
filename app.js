16891ee8c846e0991e9d403d69dce12ad5f5832c
wnsrb003 <wnsrb003@naver.com>
2021-08-03 17:35:17 +0900

    sequelize 데이터베이스 설정, passport 로그인(local, kakao), 회원가입 추가

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const passport = require('passport');
 
 dotenv.config();
 const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
+
 const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
+passportConfig(); //패스포드 설정
 app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
@@ -25,7 +30,7 @@ sequelize.sync({ force: false })
   .catch((err) => {
     console.error(err);
   });
-  
+
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -41,7 +46,11 @@ app.use(session({
   },
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', pageRouter);
+app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
   const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
--- routes/page.js
+++ routes/page.js
@@ -1,19 +1,20 @@
 var express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 var router = express.Router();
 
 router.use((req, res, next) => {
-  res.locals.user = null;
+  res.locals.user = req.user;
   res.locals.followerCount = 0;
   res.locals.followingCount = 0;
   res.locals.followerIdList = 0;
   next();
 });
 
-router.get('/profile', (req, res) => {
+router.get('/profile', isLoggedIn, (req, res) => {
   res.render('porfile', { title: '내 정보 - NodeBird' });
 });
 
-router.get('/join', (req, res) => {
+router.get('/join', isNotLoggedIn, (req, res) => {
   res.render('join', { title: '회원가입 - NodeBird' });
 });
 
